98ad7335f6eabf4fe7e6ea1906f0d7bc21c6e85f
vscardel <vscardel@gmail.com>
2024-11-28 14:58:06 -0300

    feat: add possibility to name experiment output folder

--- simulator/bin/bayesianEstimation.py
+++ simulator/bin/bayesianEstimation.py
@@ -4,10 +4,18 @@ import os
 
 
 parameters_position = {
-    
+   "ALFA": 0,
+   "BETA": 1,
+   "SLOTFRAME_INTERVAL_SIZE": 2,
+   "EPSLON_DECAY_RATE": 3,
+   "MIN_EPSLON": 4,
+   "MAX_TX_CELLS_PASSED": 5,
+   "MAX_RX_CELLS_PASSED": 6,
+   "DISCRETIZE_ENERGY_PARAMETER": 7,
+   "LAMBDA": 8
 }
 
-def efficience_function(parameters):
+def efficience_function(parameters, args):
 
     settings = None
 
@@ -19,10 +27,17 @@ def efficience_function(parameters):
     settings['execution']['numCPUs'] = args.num_cpus
     settings['execution']['numRuns'] = args.num_runs
     settings['settings']['regular']['sf_class'] = args.sched_function
+    settings['log_directory_name']= args.output_folder
+
+    #configure simulator with the parameters
+    for parameter_name,position in parameters_position.items():
+        parameter_value = parameters[position]
+        settings['settings']['regular'][parameter_name] = parameter_value
 
     with open('config.json','w') as f:
         f.write(json.dumps(settings,indent=4))
 
+    #run simulator
     os.system('python2 runSim.py')
     
 
@@ -32,6 +47,19 @@ parser.add_argument('-nc','--num_cpus', type=int, help='Number of cpu cores', re
 parser.add_argument('-nr','--num_runs', type=int, help='Number of times each combination of motes will be run', required=True)
 parser.add_argument('-cb','--combinations',type=int, nargs='+', help='combination of number of Motes', required=True)
 parser.add_argument('-sf','--sched_function', help='scheduling function alias', required=True)
-
+parser.add_argument('-of','--output_folder', help='output folder name', required=True)
 
 args = parser.parse_args()
+
+parameters = [
+    0.5,
+    0.2,
+    3,
+    0.1,
+    0.05,
+    100,
+    100,
+    0.5,
+    3
+]
+efficience = efficience_function(parameters, args)
--- simulator/bin/runExperiments.py
+++ simulator/bin/runExperiments.py
@@ -8,6 +8,8 @@ parser.add_argument('-nc','--num_cpus', type=int, help='Number of cpu cores', re
 parser.add_argument('-nr','--num_runs', type=int, help='Number of times each combination of motes will be run', required=True)
 parser.add_argument('-cb','--combinations',type=int, nargs='+', help='combination of number of Motes', required=True)
 parser.add_argument('-sf','--sched_function', help='scheduling function alias', required=True)
+parser.add_argument('-of','--output_folder', help='output folder name', required=True)
+
 
 
 args = parser.parse_args()
@@ -21,11 +23,13 @@ settings['settings']['combination']['exec_numMotes'] = args.combinations
 settings['execution']['numCPUs'] = args.num_cpus
 settings['execution']['numRuns'] = args.num_runs
 settings['settings']['regular']['sf_class'] = args.sched_function
+settings['log_directory_name']= args.output_folder
+
 
 with open('config.json','w') as f:
     f.write(json.dumps(settings,indent=4))
 
-print(f'Executando experimento com parâmetros num_cpus={args.num_cpus}, num_runs={args.num_runs}, combinations={args.combinations}, sched_function={args.sched_function}' )
+print(f'Executando experimento com parametros num_cpus={args.num_cpus}, num_runs={args.num_runs}, combinations={args.combinations}, sched_function={args.sched_function}' )
 os.system('python2 runSim.py')
 
 
